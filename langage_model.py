da10468de6026ef23ed90cafc7d672671a6fac02
vpaul <paulviossat@orange.fr>
2020-04-18 21:48:15 +0200

    Fix probabilities

--- langage_model.py
+++ langage_model.py
@@ -3,11 +3,15 @@ from operator import itemgetter
 
 def create_vocabulary(collection):
     vocabulary = dict()
-    for tokens in collection.values()
+    n = 0
+    for tokens in collection.values():
         for token in tokens:
             if not token in vocabulary:
                 vocabulary[token] = 0
             vocabulary[token] += 1
+            n += 1
+    for token in vocabulary:
+        vocabulary[token] = vocabulary[token] / n
     return vocabulary
 
 def create_language_model(collection, vocabulary, mu):
@@ -17,29 +21,32 @@ def create_language_model(collection, vocabulary, mu):
         n = len(collection[document])
         for token in collection[document]:
             if not token in index[document]:
-                index[document][token] = 1
-            else:
-                index[document][token] += 1
+                index[document][token] = 0
+            index[document][token] += 1
         for token in index[document]:
             index[document][token] = ( index[document][token] + vocabulary[token] * mu ) / ( mu + n )
+        index[document]["$length"] = n
     return index
 
-def compute_probability(query, document, index):
-    query_terms = list(map(lambda t: stem([t])[0], query.split(' ')))fromfrom utils import stem utils import stem
+def compute_probability(query, document, index, vocabulary, mu):
+    query_terms = list(map(lambda t: stem([t])[0], query.split(' ')))
     if len(query_terms) == 0:
         return 0
     p = 1
     for term in query_terms:
         if not term in index[document]:
-            return 0
+            if not term in vocabulary:
+                return 0
+            else:
+                return vocabulary[term] * mu / ( mu + index[document]["$length"])
         else:
             p *= index[document][term]
     return p
 
-def rank_document(query, index):
+def rank_document(query, index, vocabulary, mu):
     rank = []
     for document in index:
-        rank.append((document, compute_probability(query, document, index)))
-    rank.sort(key=itemgetter(1))
+        rank.append((document, compute_probability(query, document, index, vocabulary, mu)))
+    rank.sort(key=itemgetter(1), reverse=True)
     return rank
     
